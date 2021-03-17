9bcb2afc67451c7c307987385a6a4f9165b48b87
Ajay Eeralla <ajay.eeralla@gmail.com>
2021-03-16 17:07:40 -0700

    add some more keywords

--- src/extension.ts
+++ src/extension.ts
@@ -13,14 +13,14 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('saw.activate', () => {
+	/*let disposable = vscode.commands.registerCommand('saw.activate', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('SAW extension is enabled');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable);*/
 }
 
 // this method is called when your extension is deactivated
